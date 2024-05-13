64837995576096983818f4dc8aa041f4081a55e7
yokoyamahiroto1997 <168756657+yokoyamahiroto1997@users.noreply.github.com>
2024-05-13 23:17:13 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <p>
+        <?php
+        $personal_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+        print_r($personal_data);
+        ?>
+    </p>
+</body>
+
+<html>
